9a661b41f663b3fbf94f9e4dddaff6f13eff49ad
danybeltran <josuebeltran_2010@hotmail.com>
2024-01-26 19:59:23 -0600

    enh(docs): Improves docs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
-import "styles/globals.css"
+import 'styles/globals.css'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={GeistSans.className}>
+      <style suppressHydrationWarning>{`
+      code {
+        font-family: ${GeistMono.style.fontFamily} !important;
+        font-weight: ${GeistMono.style.fontWeight} !important;
+      }
+      
+      `}</style>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,25 +1,25 @@
-import React from "react"
-import { DocsThemeConfig } from "nextra-theme-docs"
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Atomic State</span>,
   project: {
-    link: "https://github.com/atomic-state/atomic-state",
+    link: 'https://github.com/atomic-state/atomic-state'
   },
   chat: {
-    link: "https://discord.com",
+    link: 'https://discord.com'
   },
   docsRepositoryBase:
-    "https://github.com/atomic-state/atomic-state.netlify.app/tree/main",
+    'https://github.com/atomic-state/atomic-state.netlify.app/tree/main',
   useNextSeoProps() {
     return {
-      titleTemplate: "%s – Atomic State",
-      description: "Atomic State is a state management library for React",
+      titleTemplate: '%s – Atomic State',
+      description: 'Atomic State is a state management library for React'
     }
   },
   footer: {
-    text: "Built with Nextra",
-  },
+    text: 'Built with Nextra'
+  }
 }
 
 export default config
